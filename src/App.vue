6ba811867bd44f9b9322fee10add12b6ca8563cf
wxf <2900856508@qq.com>
2023-07-18 17:03:34 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view />
+    <h1>黑马头条</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+<style lang="less"></style>
